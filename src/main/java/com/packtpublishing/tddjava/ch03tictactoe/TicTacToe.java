619bd7dd2ffebc16ff3dfa358e3306f1583309a9
Missael Aguilar <dev.missael@gmail.com>
2022-11-24 15:34:50 -0600

    First red-green steps

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,4 +1,9 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
 public class TicTacToe {
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("X is outside board");
+        }
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,4 +1,25 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
 }
