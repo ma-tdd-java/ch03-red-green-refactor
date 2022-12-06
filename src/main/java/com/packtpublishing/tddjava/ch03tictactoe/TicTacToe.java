85357ac353f06ca113388eb62020a4cbecd867d9
Missael Aguilar <dev.missael@gmail.com>
2022-12-06 13:04:34 -0600

    Requirement 3 (Refactoring)

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -2,6 +2,8 @@ package com.packtpublishing.tddjava.ch03tictactoe;
 
 public class TicTacToe {
 
+    private static final int SIZE = 3;
+
     private char lastPlayer = '\0';
 
     private Character[][] board = {
@@ -15,12 +17,9 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        for (int index = 0; index < 3; index++) {
-            if (board[0][index] == lastPlayer
-                    && board[1][index] == lastPlayer
-                    && board[2][index] == lastPlayer) {
-                return lastPlayer + " is the winner";
-            } }
+        if (isWin()) {
+            return lastPlayer + " is the winner";
+        }
         return "No winner";
     }
 
@@ -44,4 +43,15 @@ public class TicTacToe {
         }
         return 'X';
     }
+
+    private boolean isWin() {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] + board[1][i] + board[2][i] == (lastPlayer *
+                    SIZE)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
